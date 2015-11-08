637ce144b725c4ee665a7291d1f7546fb48fdf41
이주영 <stylejy@gmail.com>
2015-11-08 00:16:35 +0000

    Parser parboiled2 implemented and test parsing code.

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "CompilerProject"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+libraryDependencies += "org.parboiled" %% "parboiled" % "2.1.0"
--- test/test.scala
+++ test/test.scala
@@ -1,9 +1,39 @@
 /**
   * Created by stylejy on 07/11/2015.
   */
+import org.parboiled2._
+
 object test {
   def main(args: Array[String]) {
     println("test")
+    //Evaluate the argument of Calculator
+    println(new Calculator("1+(2-3*4)/5").InputLine.run())
   }
+
+  class Calculator(val input: ParserInput) extends Parser {
+    def InputLine = rule { Expression ~ EOI }
+
+    def Expression: Rule1[Int] = rule {
+      Term ~ zeroOrMore(
+        '+' ~ Term ~> ((_: Int) + _)
+          | '-' ~ Term ~> ((_: Int) - _))
+    }
+
+    def Term = rule {
+      Factor ~ zeroOrMore(
+        '*' ~ Factor ~> ((_: Int) * _)
+          | '/' ~ Factor ~> ((_: Int) / _))
+    }
+
+    def Factor = rule { Number | Parens }
+
+    def Parens = rule { '(' ~ Expression ~ ')' }
+
+    def Number = rule { capture(Digits) ~> (_.toInt) }
+
+    def Digits = rule { oneOrMore(CharPredicate.Digit) }
+  }
+
+
 
 }
