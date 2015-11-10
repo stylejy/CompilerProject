37c6d0894e8e255c890e0f376dc6e34eb8b90098
이주영 <stylejy@gmail.com>
2015-11-10 15:13:35 +0000

    Very basic bracket sensitive TestParser (accepts white spaces in String)

--- core/TestParser.scala
+++ core/TestParser.scala
@@ -10,14 +10,15 @@ class TestParser(val input: ParserInput) extends Parser {
   def InputLine = rule { Expression ~ EOI}
 
   def Expression: Rule1[Seq[Object]] = rule {
-    '(' ~ oneOrMore(Factor) ~ ')'
+    '(' ~ oneOrMore(Factor | '"' ~ Factor ~ '"') ~ ')'
   }
 
-  def Factor = rule { Letter | Number | Expression}
+  def Factor = rule { Letter | Number | Expression | Space }
 
   def Letter = rule { capture(oneOrMore(CharPredicate.Alpha)) }
 
   def Number = rule { capture(oneOrMore(CharPredicate.Digit)) }
 
+  def Space = rule { capture(oneOrMore(str(" "))) }
 }
 
--- test/test.scala
+++ test/test.scala
@@ -6,7 +6,7 @@
 object test {
   def main(args: Array[String]) {
     //Evaluate the argument of Calculator
-    val result = new TestParser("(prin(t)ln)").InputLine.run()
+    val result = new TestParser("((println (\"hihihi\")))").InputLine.run()
     if (result.isSuccess)
       println("Yes :D, it is " + result)
     else
